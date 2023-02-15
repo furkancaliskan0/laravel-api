cbc3f4dcc6cadcff8b02d0ed4e6c7a012541510f
furkanGit <furkancaliskan9801@gmail.com>
2023-02-15 17:55:27 +0100

    created seeders

--- database/seeders/GenreSeeder.php
+++ database/seeders/GenreSeeder.php
@@ -5,15 +5,19 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+use App\Models\Genre;
+
+
+
+class GenreSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-
+        //
     }
 }
