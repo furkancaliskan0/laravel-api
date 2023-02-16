eba4a628eeccb4ad2174b0e4abd1c073ade93e52
furkanGit <furkancaliskan9801@gmail.com>
2023-02-16 15:34:02 +0100

    created factories

---
+++ database/factories/MovieFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Movie>
+ */
+class MovieFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->words(rand(1, 3), true),
+            'year' => fake()->year(),
+            'cashOut' => fake()->randomNumber(5, true),
+        ];
+    }
+}
