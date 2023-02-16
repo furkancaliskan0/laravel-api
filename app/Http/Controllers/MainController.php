88a1c1a424651ba62d0bb362e499c3721a71056e
furkanGit <furkancaliskan9801@gmail.com>
2023-02-16 21:33:36 +0100

    upgrade MainController + Function + routes

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -4,11 +4,101 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Genre;
+
+use App\Models\Movie;
+
+use App\Models\Tag;
+
 class MainController extends Controller
 {
     public function home()
     {
+        $genres = Genre::all();
 
-        return view('pages.home');
+        return view('pages.home', compact('genres'));
+    }
+
+    public function movie()
+    {
+        $movies = Movie::all();
+
+        return view('pages.movie.home', compact('movies'));
+    }
+
+    public function movieCreate()
+    {
+        $genres = Genre::all();
+        $tags = Tag::all();
+
+        return view('pages.movie.create', compact('genres', "tags"));
+    }
+
+    public function movieStore(Request $request)
+    {
+
+        $data = $request->validate([
+            'name' => 'required|string|min:3',
+            'year' => 'required|integer|min:0',
+            'cashOut' => 'required|integer|min:0',
+            'genre_id' => 'required|integer|min:1',
+            'tags_id' => 'required|array',
+
+        ]);
+
+        // dd($data);
+
+        $movie = Movie::make($data);
+
+        $genre = Genre::find($data['genre_id']);
+        $movie->genre()->associate($genre);
+        $movie->save();
+
+        $tags = Tag::find($data['tags']);
+        $movie->tags()->attach($tags);
+
+        return redirect()->route('home');
+    }
+
+    public function movieDelete(Movie $movie)
+    {
+        $movie->tags()->sync([]);
+
+        $movie->delete();
+
+        return redirect()->route('home');
+    }
+
+    public function movieEdit(Movie $movie)
+    {
+        $genres = Genre::all();
+        $tags = Tag::all();
+
+        return view('pages.movie.edit', compact('movie', 'genres', 'tags'));
+    }
+
+    public function movieUpdate(Request $request, Movie $movie)
+    {
+        $data = $request->validate([
+            'name' => 'required|string|min:3',
+            'year' => 'required|integer|min:0',
+            'cashOut' => 'required|integer|min:0',
+            'genre_id' => 'required|integer|min:1',
+            'tags_id' => 'required|array',
+        ]);
+
+        $movie->update($data);
+
+        $genre = Genre::find($data['genre_id']);
+
+        $movie->genre()->associate($genre);
+
+        $movie->save();
+
+        $tags = Tag::find($data['tags']);
+
+        $movie->tags()->sync($tags);
+
+        return redirect()->route('home');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Importo il "MainController".
 use App\Http\Controllers\MainController;
 
 
@@ -9,3 +10,34 @@ use App\Http\Controllers\MainController;
 // Home Route
 Route::get('/', [MainController::class, 'home'])
     ->name('home');
+
+// Home Movie Route
+Route::get('/movie/home', [MainController::class, 'movie'])
+    ->name('movie.home');
+
+// Create  Route 
+Route::get('/movie/create', [MainController::class, 'movieCreate'])
+    ->name('movie.create');
+
+// Store  Route
+Route::post('/movie/create', [MainController::class, 'movieStore'])
+    ->name('movie.store');
+
+// Edit Route
+Route::get('/movie/edit/{movie}', [MainController::class, 'movieEdit'])
+    ->name('movie.edit');
+
+// Update Route
+Route::post('/movie/edit/{movie}', [MainController::class, 'movieUpdate'])
+    ->name('movie.update');
+
+// Delete Route
+Route::get('/movie/delete/{movie}', [MainController::class, 'movieDelete'])
+    ->name('movie.delete');
+
+
+use App\Http\Controllers\ApiController;
+
+
+
+Route::get('/api/v1/movie/all', [ApiController::class, 'movieAll']);
