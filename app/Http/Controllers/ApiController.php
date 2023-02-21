002841d97842e9545eb2719144139f50e717e0c9
furkanGit <furkancaliskan9801@gmail.com>
2023-02-21 09:37:17 +0100

    Created Genre, Store  And Delete function

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -5,18 +5,98 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Models\Movie;
+use App\Models\Genre;
+use App\Models\Tag;
+
 
 class ApiController extends Controller
 {
 
-    public function movieAll()
+    public function movieGenre()
     {
 
-        $movies = Movie::all();
+        $movies = Movie::with('tags')
+            ->orderBy('created_at', 'desc')
+            ->get();
+        $genres = Genre::all();
+        $tags = Tag::all();
 
         return response()->json([
             'success' => true,
-            'response' => $movies
+            'response' => [
+                'movies' => $movies,
+                'genres' => $genres,
+                'tags' => $tags,
+            ]
+        ]);
+    }
+
+    public function movieStore(Request $request)
+    {
+
+        $data = $request->validate([
+            'name' => 'required|string|min:3',
+            'year' => 'required|integer|min:0',
+            'cashOut' => 'required|integer|min:0',
+            'genre_id' => 'required|integer|min:1',
+            'tags_id' => 'nullable|array'
+        ]);
+
+        $genre = Genre::find($data['genre_id']);
+        $movie = Movie::make($data);
+
+        $movie->genre()->associate($genre);
+        $movie->save();
+
+        if (array_key_exists('tags_id', $data)) {
+
+            $tags = Tag::find($data['tags_id']);
+            $movie->tags()->sync($tags);
+        }
+
+        return response()->json([
+            'success' => true,
+            'response' => $movie,
+            'data' => $request->all()
+        ]);
+    }
+    public function movieUpdate(Request $request, Movie $movie)
+    {
+
+        $data = $request->validate([
+            'name' => 'required|string|min:3',
+            'year' => 'required|integer|min:0',
+            'cashOut' => 'required|integer|min:0',
+            'genre_id' => 'required|integer|min:1',
+            'tags_id' => 'nullable|array'
+        ]);
+
+        $genre = Genre::find($data['genre_id']);
+        $movie->update($data);
+        $movie->genre()->associate($genre);
+        $movie->save();
+
+        if (array_key_exists('tags_id', $data)) {
+
+            $tags = Tag::find($data['tags_id']);
+            $movie->tags()->sync($tags);
+        }
+
+        return response()->json([
+            'success' => true,
+            'response' => $movie,
+            'data' => $request->all()
+        ]);
+    }
+
+    public function movieDelete(Movie $movie)
+    {
+
+        $movie->tags()->sync([]);
+        $movie->delete();
+
+        return response()->json([
+            'success' => true
         ]);
     }
 }
