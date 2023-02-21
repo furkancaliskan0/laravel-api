f638e399eee1735fda41948e948d2f5aa18f39a8
furkanGit <furkancaliskan9801@gmail.com>
2023-02-21 16:20:13 +0100

    upgrade vue and routes

--- database/seeders/TagSeeder.php
+++ database/seeders/TagSeeder.php
@@ -15,6 +15,6 @@ class TagSeeder extends Seeder
      */
     public function run(): void
     {
-        Tag::factory()->count(50)->create();
+        Tag::factory()->count(5)->create();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ApiController;
 
 Route::get('/v1/movie/all', [ApiController::class, 'movieGenre']);
-
+Route::post('/v1/movie/store', [ApiController::class, 'movieStore']);
 Route::post('/v1/movie/update/{movie}', [ApiController::class, 'movieUpdate']);
 
-Route::delete('/v1/movie/delete/{movie}', [ApiController::class, 'movieDelete']);
+Route::get('/v1/movie/delete/{movie}', [ApiController::class, 'movieDelete']);
